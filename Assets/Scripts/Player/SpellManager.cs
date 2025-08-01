6b5a8a63074ee738b7df8f27f95db0d7a11bd172
Coco <cocochen12301@gmail.com>
2025-08-01 03:08:10 -0700

    feat: Set up audio manager and new layout

--- Assets/Scripts/Player/SpellManager.cs
+++ Assets/Scripts/Player/SpellManager.cs
@@ -2,9 +2,17 @@ using NUnit.Framework;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Windows;
 
 public class SpellManager : MonoBehaviour
 {
+    [Header("Attributes")]
+    [SerializeField, Tooltip("How much mana to start with per round")] private int defaultManaStat = 100;
+    [SerializeField, Tooltip("Current mana stats")] private int currentMana;
+
+    private PlayerInput playerInput;
+    private InputAction cast1Action;
+
     [System.Serializable]
     public class SpellSlot
     {
@@ -13,8 +21,6 @@ public class SpellManager : MonoBehaviour
     }
     [SerializeField] private List<SpellSlot> spellList;
 
-    private PlayerInput playerInput;
-    private InputAction cast1Action;
 
     private void Awake()
     {
@@ -23,13 +29,39 @@ public class SpellManager : MonoBehaviour
 
     private void Start()
     {
+        SetCurrentMana(defaultManaStat);
         cast1Action = playerInput.actions["Cast1"];
         cast1Action.performed += context => TryCast(0);
     }
+    private void OnEnable()
+    {
+        RoundStartEvent.Handler += OnRoundReset;
+    }
 
+    private void OnDisable()
+    {
+        RoundStartEvent.Handler -= OnRoundReset;
+    }
+    private void SetCurrentMana(int mana)
+    {
+        currentMana = mana;
+        new ManaValueUpdateEvent(mana).Invoke();
+    }
+    private void OnRoundReset(RoundStartEvent _)
+    {
+        SetCurrentMana(defaultManaStat);
+    }
     public void TryCast(int index)
     {
         if (index < 0 || index >= spellList.Count) return;
+
+        if (currentMana <= 0)
+        {
+            Debug.Log("Not enough mana!");
+            return;
+        }
+        Debug.Log($"Spending {spellList[index].spell.manaCost} mana");
+        SetCurrentMana(currentMana - spellList[index].spell.manaCost);
 
         SpellSlot currentSpell = spellList[index];
         
